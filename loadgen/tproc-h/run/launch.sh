6103847ab9bc237fa8532976c5dfafd8b0f409e9
Charles Wong <84283797+wongchar@users.noreply.github.com>
2024-12-10 10:47:18 -0600

    Update launch.sh

--- loadgen/tproc-h/run/launch.sh
+++ loadgen/tproc-h/run/launch.sh
@@ -10,13 +10,14 @@ IMG=tpch:run
 DBPORT=1433
 SF=30
 TH=4
+VU=4
 
 CMD="./hammerdbcli auto thruput.tcl"
 
 for ((I=1; I<=$1; I++))
 do
         DBHOST=192.168.5.${I}
-        OPTS="-e SF=${SF} -e TH=${TH} -e DBHOST=${DBHOST} -e DBPORT=${DBPORT}"
+        OPTS="-e SF=${SF} -e TH=${TH} -e DBHOST=${DBHOST} -e DBPORT=${DBPORT} -e VU=${VU}"
         NAME=tpch${I}
         LOG=${NAME}.txt
         docker run --name ${NAME} --rm ${OPTS} ${IMG} ${CMD} > ${LOG} &
